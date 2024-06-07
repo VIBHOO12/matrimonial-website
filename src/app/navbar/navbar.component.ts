df0107083b81bc9ef4cffbefca82d59731eb324b
Vibhoo Vishwakarma <121712735+VIBHOO12@users.noreply.github.com>
2024-06-07 17:02:34 +0530

    add profilesetting in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +8,35 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent {
   isLoggedIn: boolean = false; 
-
+ 
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Subscribe to router events
+    this.router.events.subscribe(event => {
+      // Check if navigation event is NavigationStart
+      if (event instanceof NavigationStart) {
+        // Check if user is not logged in and navigation is not to login page
+        if (!this.isLoggedIn && event.url !== '/') {
+          // Redirect to login page
+          this.router.navigate(['']);
+        }
+      }
+    });
+  }
 
   logout() {
     // Implement your logout logic here, like clearing user session data
     alert('Logout successfully !');
     this.router.navigate(['']); // Navigate to home page after logout
   }
+  isSubMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isSubMenuOpen = !this.isSubMenuOpen;
+    console.log('Toggle menu called. Submenu is now open: ', this.isSubMenuOpen);
+  }
 }
+
+
+
